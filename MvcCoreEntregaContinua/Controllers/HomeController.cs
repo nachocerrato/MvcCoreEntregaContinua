33f67df05d491f771088370d70bf2918e7fdd62d
nachocerrato <nacho.cr.94@gmail.com>
2022-01-24 17:33:52 +0100

    Add project files.

---
+++ MvcCoreEntregaContinua/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcCoreEntregaContinua.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreEntregaContinua.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["SALUDO"] = "Bienvenido a CI/GI Github";
+            Random random = new Random();
+            List<int> numeros = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                int num = random.Next(1, 20);
+                numeros.Add(num);
+            }
+
+            return View(numeros);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
